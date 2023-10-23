1de9005e68000cc1c084e4e08510213bb3f32ad6
Arne Martensen <arne.martensen@burohappold.com>
2023-10-23 18:17:47 +0200

    Setting Up Classes for Unit test

---
+++ .ci/unit-tests/WindLoad_Adapter_Test/TestFolder/TestTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Test.Adapter.WindLoad.TestFolder
+{
+    internal class TestTest
+    {
+        [Test]
+        public void GetStringFromEnum()
+        {
+
+
+            bool test = false;
+
+
+            if (!test)
+            {
+                test = true;
+            }
+
+
+            Assert.True(test);
+
+        }
+    }
+}
